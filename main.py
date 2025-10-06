526babe0a23bd7ae33fd35ab9c6f03da5af45061
l37sg0 <petar_1v@abv.bg>
2025-10-06 17:46:12 +0300

    Variables - #4

--- main.py
+++ main.py
@@ -8,4 +8,27 @@
 # print("20 days are " + str(50) +" minutes") # concatenating str and int
 # print(20*60*24) # math operations
 
-print(f"20 days are {20*24*60} minutes") # concatenating str and int the right way - will not work in python version 2
+# print(f"20 days are {20*24*60} minutes") # concatenating str and int the right way - will not work in python version 2
+
+to_minutes = 24 * 60
+to_seconds = to_minutes * 60
+# calculation_to_units =  to_seconds
+# unit = "seconds"
+#
+# print(f"20 days are {20 * calculation_to_units} {unit}")
+# print(f"30 days are {30 * calculation_to_units} {unit}")
+# print(f"40 days are {40 * calculation_to_units} {unit}")
+
+# calculation_to_units =  to_minutes
+# unit = "minutes"
+#
+# print(f"20 days are {20 * calculation_to_units} {unit}")
+# print(f"30 days are {30 * calculation_to_units} {unit}")
+# print(f"40 days are {40 * calculation_to_units} {unit}")
+
+calculation_to_units =  to_minutes/24
+unit = "hours"
+
+print(f"20 days are {20 * calculation_to_units} {unit}")
+print(f"30 days are {30 * calculation_to_units} {unit}")
+print(f"40 days are {40 * calculation_to_units} {unit}")
