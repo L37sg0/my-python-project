6c1e15d76f44af3ff7c328614521dfc2699b2a5f
l37sg0 <petar_1v@abv.bg>
2025-10-06 14:29:38 +0300

    Concatenating str and int the right way - #3

--- main.py
+++ main.py
@@ -5,5 +5,7 @@
 # print(2.99) # float
 # print(2 * 8) # math operations
 
-print("20 days are " + str(50) +" minutes") # concatenating str and int
-print(20*60*24) # math operations
+# print("20 days are " + str(50) +" minutes") # concatenating str and int
+# print(20*60*24) # math operations
+
+print(f"20 days are {20*24*60} minutes") # concatenating str and int the right way - will not work in python version 2
