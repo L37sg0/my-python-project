07f20dd17c8f8a053a2f192ca3e39cd773235ca4
l37sg0 <petar_1v@abv.bg>
2025-10-06 14:26:48 +0300

    Cocatenating str and int - #2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-print('200 is great number') # string
-print("200 is great number") # string
-print(2) # integer
-print(-22) # - integer
-print(2.99) # float
-print(2 * 8) # math operations
+# print('200 is great number') # string
+# print("200 is great number") # string
+# print(2) # integer
+# print(-22) # - integer
+# print(2.99) # float
+# print(2 * 8) # math operations
+
+print("20 days are " + str(50) +" minutes") # concatenating str and int
+print(20*60*24) # math operations
