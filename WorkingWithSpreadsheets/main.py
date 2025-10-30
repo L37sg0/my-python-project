bc3b1667d61f4f94d8eb47dc677c0bc623b805a0
l37sg0 <petar_1v@abv.bg>
2025-10-30 08:47:48 +0200

    List each company with respective total inventory value - #21

--- WorkingWithSpreadsheets/main.py
+++ WorkingWithSpreadsheets/main.py
@@ -5,12 +5,25 @@ product_list = inv_file["Sheet1"]
 
 # List each company with respective product count
 products_per_supplier = {}
+# List each company with respective total inventory value
+total_value_per_supplier = {}
 
 for product_row in range(product_list.min_row + 1, product_list.max_row + 1):
     supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+
+    # calculate number of products per supplier
     if supplier_name in products_per_supplier:
         products_per_supplier[supplier_name] += 1
     else:
         products_per_supplier[supplier_name] = 1
 
+    # calculate total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        total_value_per_supplier[supplier_name] += inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
 print(products_per_supplier)
+print(total_value_per_supplier)
